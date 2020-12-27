023082f8a755fd6119761b2aa389f7307ade05d0
Ktsd <oneirio115@gmail.com>
2020-12-27 15:10:48 +0700

    สร้าง Entity สำหรับ ระบบบันทึกการนัดผ่าตัด - close #3

---
+++ backend/ent/schema/surgeryappointment.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Surgeryappointment holds the schema definition for the Surgeryappointment entity.
+type Surgeryappointment struct {
+	ent.Schema
+}
+
+// Fields of the Surgeryappointment.
+func (Surgeryappointment) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Surgeryappointment.
+func (Surgeryappointment) Edges() []ent.Edge {
+	return nil
+}
